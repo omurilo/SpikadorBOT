148665101dfea2e42d26e22cd5098c8ccbe125db
Murilo Henrique <hi@omurilo.dev>
2022-04-25 00:46:38 -0300

    chore: add default cooldown to registered audios

--- index.js
+++ index.js
@@ -67,12 +67,12 @@ function redeemAudio(channel, user, rewardtype, tags, message) {
 		"1f2e5cb3-f338-412e-9898-f5df5007bb8b": {
 			instance: audio,
 			method: "play",
-			args: ["banido", channel, user, message, io],
+			args: ["banido", channel, user, message, io, 2],
 		},
 		"a4b8683f-27e4-407b-b8eb-f9ff1667157f": {
 			instance: audio,
 			method: "play",
-			args: ["peido", channel, user, message, io],
+			args: ["peido", channel, user, message, io, 2],
 		},
 		"5c63d121-e389-4713-a2a0-17f5b2840321": {
 			instance: audio,
@@ -82,62 +82,62 @@ function redeemAudio(channel, user, rewardtype, tags, message) {
 		"b2c068ff-ab17-45b0-8d54-993f3ca8b439": {
 			instance: audio,
 			method: "play",
-			args: ["matir-2", channel, user, message, io],
+			args: ["matir-2", channel, user, message, io, 2],
 		},
 		"8227be41-424a-4e53-996a-ae5d8afdc74c": {
 			instance: audio,
 			method: "play",
-			args: ["fofoca", channel, user, message, io],
+			args: ["fofoca", channel, user, message, io, 2],
 		},
 		"504aefba-2081-45a7-a725-55f301e96426": {
 			instance: audio,
 			method: "play",
-			args: ["cheguei", channel, user, message, io],
+			args: ["cheguei", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["brabo", channel, user, message, io],
+			args: ["brabo", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["chiesa", channel, user, message, io],
+			args: ["chiesa", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["noia", channel, user, message, io],
+			args: ["noia", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["palmas", channel, user, message, io],
+			args: ["palmas", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["putaria", channel, user, message, io],
+			args: ["putaria", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["satanas", channel, user, message, io],
+			args: ["satanas", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["tomar-uma", channel, user, message, io],
+			args: ["tomar-uma", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["vai-a-merda", channel, user, message, io],
+			args: ["vai-a-merda", channel, user, message, io, 2],
 		},
 		"": {
 			instance: audio,
 			method: "play",
-			args: ["viaje-e-essa", channel, user, message, io],
+			args: ["viaje-e-essa", channel, user, message, io, 2],
 		},
 	};
 
